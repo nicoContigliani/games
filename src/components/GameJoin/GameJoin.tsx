5e8dae825e63e70b9ed7ddc56093b8f4b53addfa
nicoContigliani <nico.contigliani>
2025-07-31 11:47:06 -0300

    todo

---
+++ src/components/GameJoin/GameJoin.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const GameJoin = () => {
+   //create place
+   //how is owner
+   //how is player
+    return (
+        <div>
+            GameJoin
+        </div>
+    )
+}
+
+export default GameJoin
